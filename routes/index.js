7e421b961ebeb8a1104b3dfdac5183f5a36f8126
Anto R <antragha@in.ibm.com>
2020-12-01 13:26:55 +0530

    initial

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/message', function(req, res, next) {
   //res.render('index', { title: 'Back End Services' });
-   res.json({msg: 'Welcome to Backend Services!'})
+  console.log("Got a req from frontend")
+  res.json({msg: 'Welcome to Backend Services!'})
 });
 
 module.exports = router;
